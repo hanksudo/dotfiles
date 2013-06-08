6431fc27cd5357721650c1593defad7de7c5ce69
Hank Wang <drapho@gmail.com>
2013-06-08 23:24:01 +0800

    if dotfiles directory already exists, try to update.

--- install.sh
+++ install.sh
@@ -4,7 +4,15 @@ then
     curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 fi
 
-echo "Clone dotfiles from Github..."
-git clone https://github.com/hankwang/dotfiles.git ~/.dotfiles
-cd ~/.dotfiles
+if [ -d "$HOME/.dotfiles" ]; then
+    echo "=> dotfiles is already installed in $HOME/.dotfiles, trying to update"
+    echo -ne "\r=> "
+    cd $HOME/.dotfiles && git pull
+else
+    echo "Clone dotfiles from Github..."
+    git clone https://github.com/hankwang/dotfiles.git $HOME/.dotfiles
+    cd $HOME/.dotfiles    
+fi
+
+
 script/bootstrap
