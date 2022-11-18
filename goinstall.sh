3f9c7731ecf7566812d67104a606f00e9a61bd42
Hank Wang <467745+hanksudo@users.noreply.github.com>
2022-11-18 13:58:19 +0900

    Add go tools

--- goinstall.sh
+++ goinstall.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 echo "=> Installing tools by Go... "
 echo "=> "
-go install github.com/hanksudo/gotitler
+
+go install github.com/hanksudo/gotitler@latest
+go install github.com/cosmtrek/air@latest
+go install -tags 'postgres' github.com/golang-migrate/migrate/v4/cmd/migrate@latest
